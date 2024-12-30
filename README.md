9baf3a7b5e93aae165b22cbebd258d342673a0d8
Matt Clarke <matt.clarke@ess.eu>
2024-12-30 08:42:08 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nvim_config
+
+Based on (kickstart.nvim)[https://github.com/nvim-lua/kickstart.nvim]
---
+++ lua/config/plugins/style.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd(":highlight LineNr guifg=grey")
+			vim.cmd(":highlight Comment guifg=grey")
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
+}
