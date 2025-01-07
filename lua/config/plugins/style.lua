0d5a7d4042df5039e5bbc0e0c076998c45fd4c35
Matt Clarke <matt.clarke@ess.eu>
2025-01-07 15:43:50 +0100

    improved visibility for unused code

--- lua/config/plugins/style.lua
+++ lua/config/plugins/style.lua
@@ -5,7 +5,7 @@ return {
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
 			vim.cmd(":highlight LineNr guifg=grey")
-			vim.cmd(":highlight Comment guifg=grey")
+			vim.cmd(":highlight DiagnosticUnnecessary guifg=grey")
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
