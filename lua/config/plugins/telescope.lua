1e552f94e07c0dbdff5629bc73fdd5ed04cb3b3e
Matt Clarke <matt.clarke@ess.eu>
2025-05-06 09:42:26 +0200

    Update telescope.lua

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -49,15 +49,11 @@ return {
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
-				-- pickers = {}
 				defaults = {
-					layout_config = { width = 0.85 },
+				  -- layout_strategy = "vertical",
+				  layout_config = { width = 0.90 },
 				},
+				-- pickers = {}
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown(),
