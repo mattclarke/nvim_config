23ed966a28f473b7c83b2bce8d036956b4d65926
Matt Clarke <matt.clarke@ess.eu>
2025-06-23 09:11:20 +0200

    change colour scheme

--- lua/config/plugins/style.lua
+++ lua/config/plugins/style.lua
@@ -1,13 +1,20 @@
 return {
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
+	-- 	init = function()
+	-- 		vim.cmd.colorscheme("tokyonight-night")
+	-- 		vim.cmd(":highlight LineNr guifg=grey")
+	-- 		vim.cmd(":highlight DiagnosticUnnecessary guifg=grey")
+	-- 		vim.cmd(":highlight Comment guifg=grey")
+	-- 		vim.cmd.hi("Comment gui=none")
+	-- 	end,
+	-- },
 	{
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
-			vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd(":highlight LineNr guifg=grey")
-			vim.cmd(":highlight DiagnosticUnnecessary guifg=grey")
-			vim.cmd(":highlight Comment guifg=grey")
-			vim.cmd.hi("Comment gui=none")
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
