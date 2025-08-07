17eb50a2cc127b0c79ac073e5d1390e92239d320
Matt Clarke <matt.clarke@ess.eu>
2025-08-07 07:16:04 +0200

    Update style.lua

--- lua/config/plugins/style.lua
+++ lua/config/plugins/style.lua
@@ -1,15 +1,4 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	priority = 1000, -- Make sure to load this before all the other start plugins.
-	-- 	init = function()
-	-- 		vim.cmd.colorscheme("tokyonight-night")
-	-- 		vim.cmd(":highlight LineNr guifg=grey")
-	-- 		vim.cmd(":highlight DiagnosticUnnecessary guifg=grey")
-	-- 		vim.cmd(":highlight Comment guifg=grey")
-	-- 		vim.cmd.hi("Comment gui=none")
-	-- 	end,
-	-- },
 	{
 		"catppuccin/nvim",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
